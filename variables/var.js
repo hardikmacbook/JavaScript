28d429e0489d3a5c0ea96c4a0e38159120e9468f
blaze <hardikkamaliya0000@gmail.com>
2025-07-09 14:55:13 +0530

    understanding var problem

--- variables/var.js
+++ variables/var.js
@@ -1,11 +1,28 @@
 // declare and initailization
 
-var a; // declare karna
+// var a; // declare karna
 
-var a = 12;   // declare and initailize (pahli value dena)
+// var a = 12;   // declare and initailize (pahli value dena)
 
 // window me add hota hai 
 // fuction scoped hoota hai 
 // jab hamne isko eek baar declare kiya diya, to jab dusri baar declare karenge to error nahi aayengi (aap same name se firse declare kar sakte ho!)
 
-var a = 30;
+
+// ----------------- problem with var ---------------
+
+// window me add hoga
+// var a = 30; // agar aap isse view karonge to ye windos ke ander hogaaa
+
+// function scopes 
+function demo() {
+    // var a = 30; // aab is vari ko iss function ke andar kahi bhi use kar sakte ho!!
+
+    // var tab problem karta hai jab hame usse if else me use kare like----
+
+    if (true) {
+        var a = 30; // dusri pro. language me kya hota hai ki aab hame iss var ko shirf if ke ander hi use kar sakte hai lekin JavaScript me ham abhi abhi pure function me use kar sakte hai that's resone ye baad me erro deta hai 
+    }
+}
+
+// redeclare kar sakte ho 
