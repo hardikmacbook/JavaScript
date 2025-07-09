1d9f179490988da93a2541cfea78c4839a3b20ab
blaze <hardikkamaliya0000@gmail.com>
2025-07-09 14:27:20 +0530

    updating var js

--- variables/script.js
+++
@@ -1,5 +0,0 @@
-// declare and initailization
-
-var a; // declare karna
-
-var a = 12;   // declare and initailize (pahli value dena)
---
+++ variables/var.js
@@ -0,0 +1,9 @@
+// declare and initailization
+
+var a; // declare karna
+
+var a = 12;   // declare and initailize (pahli value dena)
+
+// window me add hota hai 
+// fuction scoped hoota hai 
+// jab hamne isko eek baar declare kiya diya, to jab dusri baar declare karenge to error nahi aayengi
