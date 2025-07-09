8028f256e9ca9f0371c38cb8f50738bb4ed20154
blaze <hardikkamaliya0000@gmail.com>
2025-07-09 14:45:32 +0530

    example of const and let

---
+++ variables/const.js
@@ -0,0 +1,15 @@
+// jab ame pahle se hi pata hai ki kuch kuch values change nahi hogi to usko ham const me daal sakte hai, --- like discount kabhi change nahi hota to hame usse const me daal sakte hai -----
+
+const Discount = 10; // kabhi value change hi nahi kar sakte
+
+
+
+// more example 
+
+
+const Hus = "Lab"
+const Wife = "Laby"
+
+// so that is why ham hamesa make sure kare ki ham const ka use kare 
+
+// const Hus = "Hardik"  we got error when changing names
--- variables/let.js
+++ variables/let.js
@@ -1,2 +1,9 @@
 let a = 12; 
-let a = 12; // (aap same name se firse declare nahi kar sakte)
+//  let a = 12; // (aap same name se firse declare nahi kar sakte)
+
+// example
+
+// let hus = "Lab"
+// let wife = "Laby" 
+
+// agar aap hus ka name change karte ho to name change ho jayenga
