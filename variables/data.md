3d71670af95410c2fe3538b50191199b1887db9e
blaze <hardikkamaliya0000@gmail.com>
2025-07-09 14:21:23 +0530

    declare and initailization variables

---
+++ variables/data.md
@@ -0,0 +1,6 @@
+// var let const
+
+var a
+var a = 12;
+let a = 12;
+const a = 12;
---
+++ variables/script.js
@@ -0,0 +1,5 @@
+// declare and initailization
+
+var a; // declare karna
+
+var a = 12;   // declare and initailize (pahli value dena)
