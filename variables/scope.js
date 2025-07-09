d0b1145a8dd7c7c402d65954b0a423f067772b9f
blaze <hardikkamaliya0000@gmail.com>
2025-07-09 15:01:00 +0530

    understanging global,block,fun scopes

---
+++ variables/scope.js
@@ -0,0 +1,14 @@
+// Scope ( global, block, functional)
+
+// var a = 12; global scope jisko ham pure code me use kar sakte hai (kisi bhi {} me nahi hai )
+
+// block
+
+// {
+//     var a = 12; pure code me use hoga cuz var eek function basesd var. hai 
+// }
+
+// function hardik(){
+//     var a = 12; // function ke andar hi use hoga, pure code me use nahi hoga 
+// }
+
