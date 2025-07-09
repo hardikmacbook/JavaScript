f3fb09547974d399a44a8f7a66bcd2c0949eb31b
blaze <hardikkamaliya0000@gmail.com>
2025-07-09 18:14:57 +0530

    creating datatype

---
+++ DataType/explain.md
@@ -0,0 +1,13 @@
+primitive - aisi saari values jinko copy karne par tumhe eek real copy mil jayee
+value = string,number,boolen,null,undefined,symbol,bigint
+
+
+
+
+
+
+
+
+
+referance - inko copy karne par real copy nahi milegi but aapko reference milega parent ka 
+value = arrays,objects,functions
---
+++ DataType/script.js
@@ -0,0 +1,17 @@
+// primitive - aisi saari values jinko copy karne par tumhe eek real copy mil jayee
+// string,number,boolen,null,undefined,symbol,bigint
+
+// let a = 12;
+// let b = a;
+
+// a = a + 10; // a ni value ma +10 thase
+
+// jo tamne eem hoy ke a 22 che to pachi b pan 22 hase to eem no hoy, cuz hamne in. ke badd value me + add. karyu che to b to 10 j rase..
+
+// b = a + 10;
+
+
+
+// referance - inko copy karne par real copy nahi milegi but aapko reference milega parent ka
+// arrays,objects,functions
+// []    , {}    , ()
