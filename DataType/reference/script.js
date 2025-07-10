e08a929efe7a348d85593201d647e2902f817c95
blaze <hardikkamaliya0000@gmail.com>
2025-07-10 14:03:07 +0530

    understanding reference

---
+++ DataType/reference/script.js
@@ -0,0 +1,36 @@
+// ---------------------------- array start --------------------------//
+// agar aapko = ke baad kisi bhi tarah ka bracket mil jaye to wo reference hai - {}, [], ()
+
+// let a = [1,2,3];
+// let b = a;
+
+// b.pop() //value se last value ko remove karna 
+
+// to hona ye chaiye ki 
+// let a = [1,2,3];
+// let b = [1,2]; - but hota ye hai ki 
+
+// let a = [1,2];
+// let b = [1,2];
+
+// jab bhi aap dusri value me change karoge to wo changes frist value me bhi ho jayega
+// ---------------------------- array end --------------------------//
+
+
+
+// ---------------------------- object start --------------------------//
+
+// let a = {
+//     name: "hardik",
+// }
+
+// let b = a;
+
+// b.name = "kamaliya" // ab lagega ki b ki value ch. ho gayi hai so ya right, but a bhi change ho gaya hai 
+// ---------------------------- object end --------------------------//
+
+
+
+
+
+          
