a2736bb61e5092dd45b2c5b482abcd4a33964f6a
blaze <hardikkamaliya0000@gmail.com>
2025-07-10 14:15:16 +0530

    adding dynamic typing

---
+++ DataType/DynamicTyping.js
@@ -0,0 +1,15 @@
+// dynamic typing -> js mein static typing nahi hai and yaha opar hia dynamic typing and iska mtlb hai app data ko xhange kar sakte ho kyu ki yaha pe dynamic data type hai 
+
+
+// static example  - agar aap kisi aur lan. me work kar rahe ho to tum ye nahi kar sakte --
+
+// int a = 10; a ko int bola hai
+// a = "hardik" // fir aa string value nahi de sakte
+
+// aur dynamic me value de sakte ho like jese js me dete ho 
+
+// var a = 10; // yaha pe hamne aapne kahi nahi bola ki ye int lo 
+// a = "hardik" // fir aa string value nahi de sakte
+
+
+// this is what dynamic typing is all about 
