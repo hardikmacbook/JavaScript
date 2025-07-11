b392fd42f63776495d48595a5c8e581d8ead4d2c
blaze <hardikkamaliya0000@gmail.com>
2025-07-11 14:17:26 +0530

    some practice

---
+++ DataType/practice.js
@@ -0,0 +1,5 @@
+// Predit the result : true + false, null + 1, 5 + "5", !!undefined
+// result = true - 1 | false = 0 ==> 1 
+// result = true - 0 | false = 1 ==> 1
+// result = 55 || + ke agge string hai
+// result = false 
