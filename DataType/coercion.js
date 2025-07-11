4dbf34a48f8597149edef1e7b17c21f2936c60a4
blaze <hardikkamaliya0000@gmail.com>
2025-07-11 11:31:16 +0530

    updateing coercion

--- DataType/coercion.js
+++ DataType/coercion.js
@@ -2,3 +2,18 @@
 // trutly vs falsy values
 
 
+// Type coercion -> concept hai jisme apka eek tyoe automatically convert ho jayegaa 
+
+
+// false = 0 "" null undifined NaN document.all
+
+// if(null){
+//     // false
+// }
+
+// '5' + 1  // frist string hai aur fir number hai so baki language me nahi kar sakte but in js you can do that shit!  cuz js have Type coercion
+
+
+// "5" +  1  // answer - 51 -> tum he dekho ki  + ke aage number hai ya string
+
+// "hardik" + 1 // answer - hardik1 -> string hai to con catenation
