e4e47faf2c89df1be6754e731d8b5fbe00efe76b
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 11:25:35 +0530

    adding camparion =, ==, ===

--- operator/comparasion/camparion.js
+++ operator/comparasion/camparion.js
@@ -4,3 +4,12 @@
 // = , ==, ===, !=, !==, <=, >=, <, >, 
 
 
+
+// = // Assigns the value of the right operand to the left operand // value daalna
+
+// 12 == 13 // false equal hai kya wo dekhta hai, par iske sath ek problem hai ki agar type mismatch ho to wo convert kar deta hai
+// 12 == '12' // true, kyuki type mismatch hai, par value same hai, isliye true
+
+
+// 12 === 12 // true, type bhi check karta hai, value bhi check karta hai, dono same hone chahiye
+// 12 === '12' // false, kyuki type mismatch hai
