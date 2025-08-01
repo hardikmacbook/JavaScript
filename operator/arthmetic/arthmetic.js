b61a623ed2ea4594561db069d46b1d1f7e7a1799
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 11:14:50 +0530

    adding arthmetic and explain addtion

---
+++ operator/arthmetic/arthmetic.js
@@ -0,0 +1,5 @@
+// +, -, /, *, %, **
+
+
+// 1 + 2 = 3  // addtion shirf number ke bich me hota hai 
+// "har" + "dik" = "hardik" // jab string ho tab ho concatenation karta hai
---
+++ operator/script.js
@@ -0,0 +1,3 @@
+// arthmetic, camparion, and logical, assignment, unary, ternary operators
+
+
