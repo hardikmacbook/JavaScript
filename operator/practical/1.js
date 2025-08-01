a1a83d8758eba50b620d65e0831fc273439281aa
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 12:13:54 +0530

    adding operator practical 1

---
+++ operator/practical/1.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 20;
+
+if ( x > 5 && y < 25 ) {
+    console.log("A")
+} else {
+    console.log("B")
+}
+
+// x = 10 
+// y = 20
+
+// 10 are greater than 5 // true 
+// 20 are less than 25 // true
+// true && true = true
+
+// The condition is true, so the first block executes
+// so it will print "A"
