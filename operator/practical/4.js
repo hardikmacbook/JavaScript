12dfaba44d7a7d71063b87d58b911e865026b5a9
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 12:32:33 +0530

    updating 4.js

--- operator/practical/4.js
+++ operator/practical/4.js
@@ -1,7 +1,18 @@
-let score = 78; 
+// let score = 91; 
+// let score = 81; 
+// let score = 71; 
+// let score = 61;
+let score = 51; 
 
 let grade = score >= 90 ? "A" : 
             score >= 80 ? "B" : 
             score >= 70 ? "C" : 
-            score >= 60 ? "D" : "F";
+            score >= 60 ? "D" : 
+            "F"; // 60 ke neeche hoga to Fail
 console.log(grade);
+
+// output: A
+// output: B
+// output: C
+// output: D
+// output: F
