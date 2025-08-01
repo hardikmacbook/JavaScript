1da9c61280aa6395eb62771224614f86bc270bcc
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 11:46:06 +0530

    updating logical

--- operator/logical/logical.js
+++ operator/logical/logical.js
@@ -1,13 +1,24 @@
-// && // lefet and right dono true hai tabhi true 
-// || // dono me se koi bhi true hai tabhi true
+// && // AND // lefet and right dono true hai tabhi true 
+// || // OR // dono me se koi bhi true hai tabhi true
 // ! // not operator hai, agar true hai to false aur agar false hai to true
 
 
+// AND
 // true && true; // true
 // true && false; // false
 // false && false; // false
 // false && true; // false
 // 12>13 && 12<10; // false        // false && true 
+
+
+// OR
+
+// true || true; // true
+// true || false; // true
+// false || false; // false
+// false || true; // true
+// 12>13 || 12<10; // false        // false || true
+// 12>13 || 12<10 || 12>11; // true //
 
 
 
