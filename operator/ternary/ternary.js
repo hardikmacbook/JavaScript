8df8b1ba8c235fdcc88e68be2b4b0fcefb7feb22
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 11:52:15 +0530

    updating ternary

---
+++ operator/ternary/ternary.js
@@ -0,0 +1,21 @@
+// ? :
+
+// condition ? true hui to wo code : false ka code 
+
+
+// ternary operator ka use karte hain
+// agar humein ek hi line mein condition check karni ho aur uske according value assign karni ho
+// toh hum ternary operator ka use karte hain
+
+// syntax: condition ? valueIfTrue : valueIfFalse
+
+// let age = 18;
+// let canVote = (age >= 18) ? "Yes, you can vote." : "No, you cannot vote.";
+// console.log(canVote); // Output: Yes, you can vote.
+
+// let age = 10;
+// let canVote = (age >= 18) ? "Yes, you can vote." : "No, you cannot vote.";
+// console.log(canVote); // Output: No, you cannot vote.
+
+
+
