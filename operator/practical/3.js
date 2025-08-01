0bb4f57ead2ff1bab64bcb2fce83c0b663011767
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 12:24:59 +0530

    adding operator practical 3

---
+++ operator/practical/3.js
@@ -0,0 +1,30 @@
+// let temp = 35;
+
+// if ( temp < 30 ) {
+//     console.log("It's a cold day");
+// } else {
+//     console.log("It's a hot day");
+// }
+
+// This code checks the temperature and prints whether it's a cold or hot day
+// The condition checks if the temperature is less than 30 degrees
+// If true, it prints "It's a cold day", otherwise it prints "It's a hot day"
+// This is a simple example of using an if-else statement in JavaScript
+
+// output: "It's a hot day" if temp is 35
+// output: "It's a cold day" if temp is less than 30
+
+// final output: "It's a hot day" since temp is set to 35
+
+
+let temp = 35;
+
+if ( !(temp < 30) ) {
+    console.log("It's a cold day");
+} else {
+    console.log("It's a hot day");
+}
+ 
+
+
+
