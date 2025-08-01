329e7f090102fc922f570427d39e1ddf337783a1
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 12:16:23 +0530

    adding operator practical 2

---
+++ operator/practical/2.js
@@ -0,0 +1,15 @@
+// || ye karta hai ki agar eek bhi condition true ho to pura expression true ho jayega
+
+
+let isAdmin = false;
+let isLoggedIn = true;
+
+if (isAdmin || isLoggedIn) {
+    console.log("Access granted");
+} else {
+    console.log("Access denied");
+}
+
+// isAdmin = true
+// print "Access granted"
+
