3ab71d31b90bbde6ae9c9aa947cc1fb4dcb4492c
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 11:42:17 +0530

    adding && in logical

---
+++ operator/logical/logical.js
@@ -0,0 +1,17 @@
+// && // lefet and right dono true hai tabhi true 
+// || // dono me se koi bhi true hai tabhi true
+// ! // not operator hai, agar true hai to false aur agar false hai to true
+
+
+// true && true; // true
+// true && false; // false
+// false && false; // false
+// false && true; // false
+// 12>13 && 12<10; // false        // false && true 
+
+
+
+
+
+
+
