fca8093527e4bb428a09a4105cba5db9df8ba804
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 11:31:18 +0530

    updating camparion

--- operator/comparasion/camparion.js
+++ operator/comparasion/camparion.js
@@ -1,7 +1,15 @@
 // camparion
 
 
-// = , ==, ===, !=, !==, <=, >=, <, >, 
+// = ,
+// == not strict equality,
+// === strict equality,
+// != // not equal
+// !== // not strict equal
+// <= // less than or equal to
+// >= // greater than or equal to
+// < // less than
+// > // greater than
 
 
 
@@ -13,3 +21,14 @@
 
 // 12 === 12 // true, type bhi check karta hai, value bhi check karta hai, dono same hone chahiye
 // 12 === '12' // false, kyuki type mismatch hai
+
+
+// 12 != 13 // true, equal nahi hai, value check karta hai
+// 12 != '12' // false, kyuki value same hai, type mismatch hai, par wo value ko check karta hai
+
+// 12 !== 12 // false, type bhi check karta hai, value bhi check karta hai, dono same hone chahiye
+// 12 !== '12' // true, kyuki type mismatch hai, value bhi check karta hai, dono same hone chahiye
+
+// 12 < 13 // true, less than hai
+
+// 12 > 13 // false, greater than nahi hai
