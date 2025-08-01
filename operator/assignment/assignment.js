02649acdaede0f5fb56e104cc715ba3b14179724
blaze <hardikkamaliya0000@gmail.com>
2025-08-01 11:37:53 +0530

    updating assignment

--- operator/assignment/assignment.js
+++ operator/assignment/assignment.js
@@ -9,3 +9,24 @@
 // **=
 
 
+// let a = 10; // initial value
+
+// a = a + 5; // reassigning value
+// console.log(a); // 15
+
+// a += 5; // equivalent to a = a + 5
+// console.log(a); // 20
+
+
+// a -= 5; // equivalent to a = a - 5
+// console.log(a); // 15
+
+// a *= 2; // equivalent to a = a * 2
+// console.log(a); // 30
+
+// a /= 3; // equivalent to a = a / 3
+// console.log(a); // 10
+
+// a %= 3; // equivalent to a = a % 3
+// console.log(a); // 1
+
