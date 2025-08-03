e7b60b8d4fa089aa6ba393dad3b9ef67cbdb8f52
blaze <hardikkamaliya0000@gmail.com>
2025-08-03 09:55:38 +0530

    adding switch case

---
+++ control-flow/switch-case.js
@@ -0,0 +1,24 @@
+// switch case 
+
+
+let a = 10;
+
+switch (a){
+    case 1:
+        console.log("a is 1");
+        break;
+    case 2:
+        console.log("a is 2");
+        break;  
+    case 3:
+        console.log("a is 3");
+        break;
+    case 4:
+        console.log("a is 4");
+        break;
+    case 5:
+        console.log("a is 5");
+        break;
+    default:
+        console.log("a is not 1, 2, 3, 4, or 5");
+}
