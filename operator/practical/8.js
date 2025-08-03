f6256b58acc60bef691fcb1b484b3f20f5b357a3
blaze <hardikkamaliya0000@gmail.com>
2025-08-03 09:37:45 +0530

    adding 8.js

---
+++ operator/practical/8.js
@@ -0,0 +1,7 @@
+let h = 1
+
+console.log(++h); // Output: 2
+
+console.log(h++); // Output: 2 // ++ baad me hai to h abhi bhi 2 hai aur jab h dusri baar print hoga to 3 ho jayega
+
+console.log(h); // Output: 3
