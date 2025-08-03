499510a2bfbe07b5745efffdbe29b45e9dab5f13
blaze <hardikkamaliya0000@gmail.com>
2025-08-03 10:14:53 +0530

    updating practical in control-flow

--- control-flow/1.js
+++ control-flow/1.js
@@ -1,28 +1,32 @@
 function getGrade(score){
     if (score >= 90 && score <= 100) {
-        return 'A';
+        return 'A+';
     }
     else if (score >= 80 && score < 90) {
-        return 'B';
+        return 'A';
     }
     else if (score >= 70 && score < 80) {
-        return 'C';
+        return 'B';
     }
     else if (score >= 60 && score < 70) {
+        return 'C';
+    }
+    else if (score >= 33 && score < 59) {
         return 'D';
     }
-    else if (score >= 0 && score < 60) {
-        return 'F';
+    else if (score >= 0 && score < 32) {
+        return 'Fail';
     }
     else {
         return 'Invalid score';
     }
 }
 
-console.log(getGrade(95)); // Output: A
-console.log(getGrade(85)); // Output: B
-console.log(getGrade(75)); // Output: C
-console.log(getGrade(65)); // Output: D
-console.log(getGrade(55)); // Output: F
+console.log(getGrade(95)); // Output: A+
+console.log(getGrade(85)); // Output: A
+console.log(getGrade(75)); // Output: B
+console.log(getGrade(65)); // Output: C
+console.log(getGrade(50)); // Output: D
+console.log(getGrade(20)); // Output: Fail
 console.log(getGrade(-5)); // Output: Invalid score
 console.log(getGrade(105)); // Output: Invalid score
